4383e158ef6f742270e5b2ca7d4fe0165d8eb1cc
SunilDongarwar <dongarwar.sunil@gmail.com>
2024-12-04 01:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-myapp = Flask(__name__)
-@myapp.route("/")
+app = Flask(__name__)
+@app.route("/")
 def helloworld():
     return "Hello World! Lets Start Contributing Myapp"
 if __name__ == "__main__":
-    myapp.run()
+    app.run()
